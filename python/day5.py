cf1882280bf373b5631ec291da3af3d7cc64f5b5
Manuel Hüsers <manuel.huesers@uni-oldenburg.de>
2017-12-06 21:17:28 +0100

    Day 5

---
+++ python/day5.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import InputHelper as IH
+
+
+# Day 2 Part 1
+def solve1(lines):
+	return compute(convert(lines), lambda i: i + 1)
+
+# Day 2 Part 2
+def solve2(lines):
+	return compute(convert(lines), lambda i: i + 1 if i < 3 else i - 1)
+
+
+def convert(lines):
+	return [ int(line) for line in lines ]
+
+def compute(lines, cond):
+	counter = 0
+	index = 0
+	while index < len(lines):
+		index = index if index > 0 else 0
+		newindex = lines[index]
+		lines[index] = cond(lines[index])
+		index += newindex
+		counter += 1
+
+	return counter
+
+# Read from stdin or else from input file
+data = IH.InputHelper(5).readlines()
+print("Part 1: " + str(solve1(data)))
+print("Part 2: " + str(solve2(data)))
